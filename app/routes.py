2cd80223b803c457d43a81001b9647e73c94c7d8
Thomas Yeboah <asomani528@gmail.com>
2025-05-17 00:18:51 +0000

    all

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 import pandas as pd
+from app import app
+
 
 main_bp = Blueprint('main', __name__)
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
+
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    app.run(debug=True)
